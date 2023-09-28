e61915ed8f470fd92849011a0ef7adf7395c463c
mihaib1 <baiasumihai07@yahoo.com>
2023-09-28 23:20:36 +0300

    Add modal with SOME functionality

--- script.js
+++ script.js
@@ -187,17 +187,49 @@ testBtn = document.getElementById("test");
 testBtn.addEventListener("click", function(e){});
 
 
-// de aranjat pagina, de adaugat buton de start game dupa ce a introdus numele
-// de adaugat optiune de ales intre player 1 si player 2
-// eventual de facut un AI care sa faca o miscare legala random
 
 
-// pot face un loc unde va aparea o eroare de cate ori utilizatorul incearca sa faca o miscare interzisa.
-
-// Comentarii modul game()
-        // putem face 2 butoane care sa seteze simbolul - facut butoane, trebuie pusa actiunea pe ele
-        // putem face un script care va alege random ce simbol va primi utilizatorul
-        // putem seta simbolul in functie de dificultate (aici va fi nevoie de 3 butoane - Easy, Medium si Hard), iar in functie de dificultate alegem butonul
-        // momentan o sa-l las pe X pentru testare;
+// create a div where an error appears if player makes a forbidden move (timeout so that error disappears on click or after 3s)
 
 
+/// MODAL Functions
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal -- to be removed
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+var symbolButtons = document.querySelectorAll('[class="symbolBtn"]');
+symbolButtons.forEach(function(button){
+    button.onclick = function(e){
+        console.log(e.srcElement.getAttribute("symbol"));
+        playerSelection = document.getElementsByClassName("selected");
+        if(playerSelection.length > 0){
+            playerSelection[0].classList.remove("selected");
+        }
+        e.srcElement.classList.add("selected");
+        // set player1 symbol to the clicked element -> show "confirm" button
+        // add class "selected" which changes the color of the button
+    }
+})
